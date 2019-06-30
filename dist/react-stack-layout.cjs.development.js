ee88dfc8b14f0c633a9af391e973f303def6ae49
Janusz <jtwozniak@gmail.com>
2019-06-30 19:01:44 +0100

    Initial generation of the project

---
+++ dist/react-stack-layout.cjs.development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var React = _interopDefault(require('react'));
+
+var Thing = function Thing() {
+  return React.createElement("div", null, "the snozzberries taste like snozzberries");
+};
+
+exports.Thing = Thing;
+//# sourceMappingURL=react-stack-layout.cjs.development.js.map
---
+++ dist/react-stack-layout.esm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+var Thing = function Thing() {
+  return React.createElement("div", null, "the snozzberries taste like snozzberries");
+};
+
+export { Thing };
+//# sourceMappingURL=react-stack-layout.esm.js.map
